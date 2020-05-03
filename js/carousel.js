cb1d088136d49d2e5971e8f6defc268e28ed9325
Tiago-Perri <tiagocnsr@gmail.com>
2020-05-03 03:52:38 -0300

    versão 1.0

--- js/carousel.js
+++ js/carousel.js
@@ -2,7 +2,7 @@ $('.carousel').slick({
     arrows:false,
     dots:true,
     infinite: true,
-    autoplay: true,
+    autoplay: false,
     speed: 900
   });
   
--- js/scroll.js
+++ js/scroll.js
@@ -8,3 +8,14 @@ $('.nav-header a').click(function(e){
         scrollTop: targetOffset - menuHeight
     }, 900);
 });
+
+$('.mapasite ul li a').click(function(e){
+    e.preventDefault();
+    var id = $(this).attr('href'),
+        targetOffset = $(id).offset().top,
+        menuHeight = $('nav-header').innerHeight();
+
+    $('html, body').animate({
+        scrollTop: targetOffset - menuHeight
+    }, 900);
+});
