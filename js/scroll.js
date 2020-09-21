4a80120eb26f4064fdd3af287701b38664f4928a
TiagoPerri <tiagocnsr@gmail.com>
2020-09-21 15:40:53 -0300

    add btn HOME

--- js/scroll.js
+++ js/scroll.js
@@ -19,3 +19,14 @@ $('.mapasite ul li a').click(function(e){
         scrollTop: targetOffset - menuHeight
     }, 900);
 });
+
+$('.btn-fixed-home').click(function(e){
+    e.preventDefault();
+    var id = $(this).attr('href'),
+        targetOffset = $(id).offset().top,
+        menuHeight = $('nav-header').innerHeight();
+
+    $('html, body').animate({
+        scrollTop: targetOffset - menuHeight
+    }, 900);
+});
